948a1ce82f6c880bcf679f6148d80046300c6a07
Dhruv Ratmele <129955117+Somgester@users.noreply.github.com>
2023-12-12 14:50:12 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const creditCardDetails = document.getElementById('cardDet');
-    const creditCardRadio = document.getElementById('credit-card');
+const creditCardRadio = document.getElementById('credit-card');
+
+let amt = document.getElementById('billAmt').value;
+
 
     const UPIDetails = document.getElementById('upiId');
     const UPIRadio = document.getElementById('pay');
 
-
     creditCardRadio.addEventListener('change', () => {
       if (creditCardRadio.checked) {
         creditCardDetails.style.display = 'block';
-      } if (UPIRadio.checked) {
-        UPIDetails.style.display = 'block';
-      }
+       }
       else {
         creditCardDetails.style.display = 'none';
         UPIDetails.style.display = 'none';
       }
     });
+function upi() {
+  if (UPIRadio.checked) {
+    UPIDetails.style.display = 'block';
+  }
+  else {
+    UPIDetails.style.display = 'none';
+  }
+}
+
+function paid() {
+    alert("Payment Successful");
+}
